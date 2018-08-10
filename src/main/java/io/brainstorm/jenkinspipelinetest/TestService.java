144479afff2b07546111c0adfd6ff74a1d3c66ce
Chetan Lohar <chetan.lohar@enterprisedb.com>
2018-08-10 12:40:19 +0530

    Poll SCM test3 commit

--- src/main/java/io/brainstorm/jenkinspipelinetest/TestService.java
+++ src/main/java/io/brainstorm/jenkinspipelinetest/TestService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 public class TestService {
 
     public String getData(){
-        return "Jenking Pipeline Test Poll SCM....Test2!";
+        return "Jenking Pipeline Test Poll SCM....Test3!";
     }
 
     public Integer add(Integer a,Integer b)
